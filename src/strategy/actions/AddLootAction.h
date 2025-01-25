ba3dff8a206c831ce4e590a8b6a36418dc4b1bfd
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-01-24 22:26:55 -0600

    Update AddLootAction.h

--- src/strategy/actions/AddLootAction.h
+++ src/strategy/actions/AddLootAction.h
@@ -3,11 +3,6 @@
  * and/or modify it under version 2 of the License, or (at your option), any later version.
  */
 
-/**
- * @file AddLootAction.h
- * Defines actions for collecting loot in the playerbot system
- */
-
 #ifndef _PLAYERBOT_ADDLOOTACTION_H
 #define _PLAYERBOT_ADDLOOTACTION_H
 
@@ -16,43 +11,26 @@
 class ObjectGuid;
 class PlayerbotAI;
 
-/**
- * @brief Action for adding a single loot object
- */
 class AddLootAction : public Action
 {
     public:
         AddLootAction(PlayerbotAI* botAI) : Action(botAI, "add loot") {}
-        bool Execute(Event const& event) override;
+        bool Execute(Event event) override;
         bool isUseful() override { return true; }
 };
 
-/**
- * @brief Action for adding all nearby lootable objects
- */
 class AddAllLootAction : public Action
 {
     public:
         AddAllLootAction(PlayerbotAI* botAI, std::string const& name = "add all loot") 
             : Action(botAI, name) {}
-        bool Execute(Event const& event) override;
+        bool Execute(Event event) override;
         bool isUseful() override { return true; }
 
     protected:
-        /**
-         * @brief Adds a specific object to loot stack
-         * @param guid Object to be looted
-         * @return true if successfully added
-         */
-        virtual bool AddLoot(ObjectGuid const& guid)
-        {
-            return AI_VALUE(LootObjectStack*, "available loot")->Add(guid);
-        }
+        virtual bool AddLoot(ObjectGuid const& guid);
 };
 
-/**
- * @brief Specialized action for gathering profession resources
- */
 class AddGatheringLootAction : public AddAllLootAction
 {
     public:
